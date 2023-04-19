f26b9f892e580a4d9a49683cb812f038d42909b7
kema-ray <rachelkemuma99@gmail.com>
2023-04-19 06:13:30 +0300

    fix code

--- models/place.py
+++ models/place.py
@@ -8,6 +8,16 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Table
+from os import getenv
+from models.amenity import Amenity
+from models.review import Review
+import models
+
+table = Table("place_amenity", Base.metadata, Column("place_id", String(60),
+              ForeignKey("places_id"),  primary_key=True, nullable=False),
+              Column("amenity_id", String(60),
+              ForeignKey("amenities.id"), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -23,3 +33,30 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False)
+    amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """Get a list of all linked Reviews."""
+            review_list = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            """Get or set linked Amenities."""
+            amenity_list = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, value):
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
