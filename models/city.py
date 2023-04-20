c351cc6507a321c86bd7e84a35e25d99273f4dc8
kema-ray <rachelkemuma99@gmail.com>
2023-04-20 05:50:12 +0300

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -7,16 +7,16 @@ from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-
+import os
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     # if storage_type == 'db':
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     places = relationship('Place', backref='cities',
-                              cascade='all, delete, delete-orphan')
+                              cascade='all, delete, delete-orphan') if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
     # else:
     #     name = ''
     #     state_id = ''
--- models/user.py
+++ models/user.py
@@ -6,17 +6,17 @@ from models import storage_type
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
-
+import os
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    email = Column(String(128), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    password = Column(String(128), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    first_name = Column(String(128), nullable=True) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    last_name = Column(String(128), nullable=True) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     places = relationship('Place', backref='user',
-                           cascade='delete')
+            cascade='delete') if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
     reviews = relationship('Review', backref='user',
-                            cascade='delete')
+            cascade='delete') if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
     
